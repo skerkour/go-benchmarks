c99591c1a7c5bb922c858320e0447368ebd7d19b
dev <>
2024-01-18 17:03:28 +0000

    encryption_aead: Add XChaCha20-SHA256 :)

--- encryption_aead/encryption_test.go
+++ encryption_aead/encryption_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/bloom42/stdx/xchacha20blake3"
+	"github.com/bloom42/stdx/xchacha20sha256"
 	"github.com/skerkour/go-benchmarks/utils"
 	"golang.org/x/crypto/chacha20"
 	"golang.org/x/crypto/chacha20poly1305"
@@ -47,6 +48,7 @@ func BenchmarkEncryptAEAD(b *testing.B) {
 	for _, size := range BENCHMARKS {
 		benchmarkEncrypt(b, size, "XChaCha20_BLAKE3", newXChaCha20Blake3Cipher(b, xChaCha20Key), xChaCha20Nonce, additionalData)
 		benchmarkEncrypt(b, size, "XChaCha20_Poly1305", newXChaCha20Poly1305Cipher(b, xChaCha20Key), xChaCha20Nonce, additionalData)
+		benchmarkEncrypt(b, size, "XChaCha20_SHA256", newXChaCha20Sha256Cipher(b, xChaCha20Key), xChaCha20Nonce, additionalData)
 		benchmarkEncrypt(b, size, "ChaCha20_Poly1305", newChaCha20Poly1305Cipher(b, chaCha20Key), chaCha20Nonce, additionalData)
 		benchmarkEncrypt(b, size, "AES_128_GCM", newAesGcmCipher(b, aes128GcmKey), aes128GcmNonce, additionalData)
 		benchmarkEncrypt(b, size, "AES_256_GCM", newAesGcmCipher(b, aes256GcmKey), aes256GcmNonce, additionalData)
@@ -71,6 +73,7 @@ func BenchmarkDecryptAEAD(b *testing.B) {
 	for _, size := range BENCHMARKS {
 		benchmarkDecrypt(b, size, "XChaCha20_BLAKE3", newXChaCha20Blake3Cipher(b, xChaCha20Key), xChaCha20Nonce, additionalData)
 		benchmarkDecrypt(b, size, "XChaCha20_Poly1305", newXChaCha20Poly1305Cipher(b, xChaCha20Key), xChaCha20Nonce, additionalData)
+		benchmarkDecrypt(b, size, "XChaCha20_SHA256", newXChaCha20Sha256Cipher(b, xChaCha20Key), xChaCha20Nonce, additionalData)
 		benchmarkDecrypt(b, size, "ChaCha20_Poly1305", newChaCha20Poly1305Cipher(b, chaCha20Key), chaCha20Nonce, additionalData)
 		benchmarkDecrypt(b, size, "AES_128_GCM", newAesGcmCipher(b, aes128GcmKey), aes128GcmNonce, additionalData)
 		benchmarkDecrypt(b, size, "AES_256_GCM", newAesGcmCipher(b, aes256GcmKey), aes256GcmNonce, additionalData)
@@ -148,6 +151,29 @@ func (cipher xChaCha20Poly1305Cipher) Encrypt(dst, nonce, plaintext, additionalD
 }
 
 func (cipher xChaCha20Poly1305Cipher) Decrypt(dst, nonce, ciphertext, additionalData []byte) {
+	_, _ = cipher.cipher.Open(dst, nonce, ciphertext, additionalData)
+}
+
+type xChaCha20Sha256Cipher struct {
+	cipher cipher.AEAD
+}
+
+func newXChaCha20Sha256Cipher(b *testing.B, key []byte) xChaCha20Sha256Cipher {
+	cipher, err := xchacha20sha256.New(key)
+	if err != nil {
+		b.Error(err)
+	}
+
+	return xChaCha20Sha256Cipher{
+		cipher: cipher,
+	}
+}
+
+func (cipher xChaCha20Sha256Cipher) Encrypt(dst, nonce, plaintext, additionalData []byte) []byte {
+	return cipher.cipher.Seal(dst, nonce, plaintext, additionalData)
+}
+
+func (cipher xChaCha20Sha256Cipher) Decrypt(dst, nonce, ciphertext, additionalData []byte) {
 	_, _ = cipher.cipher.Open(dst, nonce, ciphertext, additionalData)
 }
 
